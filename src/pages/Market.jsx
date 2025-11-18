d74686311b195c3cbde613e1bcf3745983a71777
AyeshaMalak <ayeshamalak34@gmail.com>
2025-11-18 23:14:47 +0500

    Update Market.jsx

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SideTextSidebar from "../components/SideTextSidebar";
+
 
 const Market = () => {
   return (
     <div className="relative bg-white min-h-screen font-sans">
 
-      {/* Side Text Sidebar */}
-      <SideTextSidebar />
+      
 
       {/* Header */}
       <div className="fixed top-0 left-0 right-0 w-full bg-white px-4 py-1.5 shadow-md z-50">
@@ -82,3 +81,4 @@ const Section = ({ title, text }) => (
 );
 
 export default Market;
+
